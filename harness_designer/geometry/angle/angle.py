Fix rotation matrix convention to match OpenGL column-vector standard
Co-authored-by: kdschlosser <10932297+kdschlosser@users.noreply.github.com>
Agent-Logs-Url: https://github.com/HarnessDesigner/HarnessDesigner/sessions/f1a53703-5ebf-4740-acbe-febbc03a50ee

harness_designer/geometry/angle/angle.py
@@ -330,9 +330,13 @@ class Angle(metaclass=AngleMeta):
 
     def __imatmul__(self, other: Union[np.ndarray, _point.Point]) -> np.ndarray:
         if isinstance(other, np.ndarray):
-            other @= self._matrix.T
+            if other.ndim == 1:
+                result = self._matrix @ other
+                other[:] = result
+            else:
+                other[:] = (self._matrix @ other.T).T
         elif isinstance(other, _point.Point):
-            values = other.as_numpy @ self._matrix.T
+            values = self._matrix @ other.as_numpy
 
             x = float(values[0])
             y = float(values[1])
@@ -350,9 +354,12 @@ class Angle(metaclass=AngleMeta):
 
     def __matmul__(self, other: Union[np.ndarray, _point.Point]) -> np.ndarray:
         if isinstance(other, np.ndarray):
-            other = other @ self._matrix.T
+            if other.ndim == 1:
+                return self._matrix @ other
+            else:
+                return (self._matrix @ other.T).T
         elif isinstance(other, _point.Point):
-            values = other.as_numpy @ self._matrix.T
+            values = self._matrix @ other.as_numpy
             x = float(values[0])
             y = float(values[1])
             z = float(values[2])

harness_designer/objects/objects3d/base3d.py
@@ -94,12 +94,24 @@ class Base3D:
             p1, p2 = _utils.compute_aabb(self._data[0])
             aabb = _utils.adjust_aabb(np.array([p1.as_float, p2.as_float], dtype=np.float64))
         else:
-            local_aabb = self._vbo.local_aabb * self._scale
-            local_aabb @= self._angle
+            local_min = self._vbo.local_aabb[0]
+            local_max = self._vbo.local_aabb[1]
 
-            local_aabb += self._position
+            x1, y1, z1 = local_min
+            x2, y2, z2 = local_max
 
-            aabb = _utils.adjust_aabb(local_aabb)
+            corners = np.array([
+                [x1, y1, z1], [x1, y1, z2],
+                [x1, y2, z1], [x1, y2, z2],
+                [x2, y1, z1], [x2, y1, z2],
+                [x2, y2, z1], [x2, y2, z2]
+            ], dtype=np.float64)
+
+            corners *= self._scale.as_numpy
+            corners @= self._angle
+            corners += self._position.as_numpy
+
+            aabb = _utils.adjust_aabb(corners)
 
         for i in range(2):
             for j in range(3):